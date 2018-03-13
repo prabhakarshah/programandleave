e05935d7c6f2b170e50a1f4e30f988d050f218c4
Prabhakar Kumar Shah <prabhakar3333@gmail.com>
2018-03-13 07:48:53 +0530

    reverse linked list
    Added and tested reverse4 linked list

--- Source.cpp
+++ Source.cpp
@@ -1,61 +1,66 @@
+// ConsoleApplication4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
-struct Node{
+
+struct Node {
 	int data;
 	Node* next;
-	Node(int data){
+	Node(int data) {
 		this->data = data;
 		next = nullptr;
 	}
 };
 
-void pushNode(Node* &head, int data){
+void pushNode(Node* &head, int data) {
 	Node* newNode = new Node(data);
 	newNode->next = head;
 	head = newNode;
 }
 
-void print(Node* head){
-	while (head != nullptr){
-		cout << head->data<<", ";
+void print(Node* head) {
+	while (head != nullptr) {
+		cout << head->data << ", ";
 		head = head->next;
 	}
 	cout << endl;
 }
 
-void moveNode(Node* &src, Node* &dest){
+void moveNode(Node* &dest, Node* &src) {
 	Node* temp = src;
 	src = src->next;
 	dest->next = temp;
 	dest = temp;
 }
 //Merge two lists given in sorted order
-Node* mergeList(Node* &list1, Node* &list2){
+Node* mergeList(Node* &list1, Node* &list2) {
 	Node dummy(0);
 	Node* outList = &dummy;
-	while (true){
-		if (list1 == nullptr){
+	while (true) {
+		if (list1 == nullptr) {
 			outList->next = list2;
 			break;
 		}
-		if (list2 == nullptr){
+		if (list2 == nullptr) {
 			outList->next = list1;
 		}
 
-		if (list1->data <= list2->data){
-			moveNode(outList->next, list1);
+		if (list1->data <= list2->data) {
+			moveNode(outList, list1);
 		}
 		else
-			moveNode(outList->next, list2);
+			moveNode(outList, list2);
 
 		outList = outList->next;
 	}
 	return outList;
 }
 
-Node* makeList(){
-	Node *head;
+Node* makeList() {
+	Node *head =  nullptr;
 	pushNode(head, 1);
 	pushNode(head, 2);
 	pushNode(head, 3);
@@ -66,9 +71,48 @@ Node* makeList(){
 	return head;
 }
 
-int main(){
+Node* makeList1() {
+	Node *head = nullptr;
+	pushNode(head, 1);
+	pushNode(head, 3);
+	pushNode(head, 4);
+
+	return head;
+}
+void reverse(Node* &head) {
+	Node *current = head;
+	Node* prev = nullptr, *next = nullptr;
+
+	while (true)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+		if (current == nullptr)
+			break;
+
+	}
+
+	head = prev;
+}
+
+Node* makeList2() {
+	Node *head = nullptr;
+	pushNode(head, 2);
+	pushNode(head, 5);
+	pushNode(head, 7);
+
+	return head;
+}
+
+int main() {
 	Node* head = makeList();
 	print(head);
+	reverse(head);
+	print(head);
+
 	return 0;
 
 }
+
