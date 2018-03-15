0bb643c63093d5fecec9a607b149b5fbf17e5874
Prabhakar Kumar Shah <prabhakar3333@gmail.com>
2018-03-15 04:46:54 -0700

    getsum implemented

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <iostream>
 #include <vector>
 #include <array>
 
@@ -9,15 +9,39 @@ class SegmentTree
 {
 private:
 	std::array<int, 256> _tree;
+	int _size = 0;
 public:
 	SegmentTree(vector<int> &arr)
 	{
 		constructSTUtil(arr, 0, arr.size() - 1, 0);
+		_size = arr.size();
 	}
-	int getMax(int left, int right)
+	
+	int getSum(int queryStart, int queryEnd)
 	{
-
+		return getSum(0, _size-1, queryStart, queryEnd);
 	}
+
+private:
+
+	int getSum(int segmentStart, int segmentEnd, int queryStart, int queryEnd, int si=0)
+	{
+		// If segmentEndgment of this node is a part of given range, then return
+		// the sum of the segmentEndgment
+		if (queryStart <= segmentStart && queryEnd >= segmentEnd)
+			return _tree[si];
+
+		// If segmentEndgment of this node is outside the given range
+		if (segmentEnd < queryStart || segmentStart > queryEnd)
+			return 0;
+
+		// If a part of this segmentEndgment overlaps with the given range
+		int mid = getMid(segmentStart, segmentEnd);
+
+		return getSum(segmentStart, mid, queryStart, queryEnd, 2 * si + 1) +
+			getSum(mid + 1, segmentEnd, queryStart, queryEnd, 2 * si + 2);
+	}
+
 	int getMid(int s, int e) { return s + (e - s) / 2; }
 
 	int constructSTUtil(vector<int> arr, int segmentStart, int segmentEnd, int si)
@@ -31,6 +55,7 @@ public:
 		int mid = getMid(segmentStart, segmentEnd);
 		_tree[si] = constructSTUtil(arr, segmentStart, mid,  si * 2 + 1) +
 			constructSTUtil(arr, mid + 1, segmentEnd,  si * 2 + 2);
+
 		return _tree[si];
 	}
 };
@@ -39,5 +64,6 @@ int main()
 {
 	vector<int> arr{ 1, 3, 5, 7, 9, 11 };
 	SegmentTree st(arr);
+	std::cout << st.getSum(1, 3);
 	return 0;
 }
