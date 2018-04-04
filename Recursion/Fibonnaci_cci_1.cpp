ad9831d76127aa2c3f331e5f8c39defd191d3f1d
Prabhakar Kumar Shah <prabhakar3333@gmail.com>
2018-04-04 10:55:24 +0530

    soilutiuin

--- Recursion/Fibonnaci_cci_1.cpp
+++ Recursion/Fibonnaci_cci_1.cpp
@@ -1,2 +1,19 @@
 //Problem HackerRank Fibonnaci
 //https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) {
+     if( n ==0 || n==1)
+         return n;
+    
+    return fibonacci(n-1)+ fibonacci(n-2);
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
